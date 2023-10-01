42a4cf16040440035c6c0c2bed6981149c15f26a
Dustin Stacy <dustin.stacy@outlook.com>
2023-10-01 18:22:25 -0400

    Update RandomIPFSNFT deploy script

--- deploy/02-deploy-random-ipfs-nft.ts
+++ deploy/02-deploy-random-ipfs-nft.ts
@@ -1,20 +1,51 @@
-import { network } from 'hardhat'
+import { ethers, network } from 'hardhat'
 import { devChains, networkConfig } from '../helper-hardhat-config'
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 import verify from '../utils/verify'
+import { VRFCoordinatorV2Mock } from '../typechain-types'
 
 const deployRandomIPFSNFT: DeployFunction = async ({ getNamedAccounts, deployments }) => {
-    const { deploy, log } = deployments
+    const { deploy, log, get } = deployments
     const { deployer } = await getNamedAccounts()
 
-    log('-------------------------')
-    const args: any[] = []
+    let vrfCoordinatorV2Address: string | undefined
+    let vrfCoordinatorV2Mock: VRFCoordinatorV2Mock
+    let subscriptionId: string | undefined
+
+    if (devChains.includes(network.name)) {
+        const vrfCoordinatorV2MockDeployment = await get('VRFCoordinatorV2Mock')
+        vrfCoordinatorV2Address = vrfCoordinatorV2MockDeployment.address
+        vrfCoordinatorV2Mock = await ethers.getContractAt(
+            'VRFCoordinatorV2Mock',
+            vrfCoordinatorV2Address
+        )
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait()
+        subscriptionId = txReceipt!.logs[0].topics[1]
+    } else {
+        vrfCoordinatorV2Address = networkConfig[network.name]['vrfCoordinatorV2']
+        subscriptionId = networkConfig[network.name]['subscriptionId']
+    }
+
+    const args: any[] = [
+        vrfCoordinatorV2Address,
+        networkConfig[network.name]['gasLane'],
+        subscriptionId,
+        networkConfig[network.name]['callbackGasLimit'],
+        networkConfig[network.name]['dogTokenURIs'],
+        networkConfig[network.name]['mintFee'],
+    ]
+
     const randomIPFSNFT = await deploy('RandomIPFSNFT', {
         from: deployer,
         args: args,
         log: true,
-        waitConfirmations: networkConfig[network.name].blockConfirmations,
+        waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
     })
+
+    if (devChains.includes(network.name)) {
+        await vrfCoordinatorV2Mock!.addConsumer(subscriptionId as string, randomIPFSNFT.address)
+    }
 
     if (!devChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log('Verifying...')
