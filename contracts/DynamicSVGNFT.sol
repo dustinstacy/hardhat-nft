e3d4cea2a91df08a29287dff4585ca2955027679
Dustin Stacy <dustin.stacy@outlook.com>
2023-10-04 12:49:02 -0400

    add mint function

--- contracts/DynamicSVGNFT.sol
+++ contracts/DynamicSVGNFT.sol
@@ -1,9 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DynamicSVGNFT is ERC721URIStorage, Ownable {
-    constructor() ERC721("DynamicFaces", "FACE") {}
+contract DynamicSVGNFT is ERC721, Ownable {
+    uint256 public _tokenCounter;
+
+    constructor() ERC721("Dynamic Faces", "FACE") {
+        _tokenCounter = 0;
+    }
+
+    function mintNFT() public {
+        _safeMint(msg.sender, _tokenCounter);
+        _tokenCounter = _tokenCounter + 1;
+    }
+
+    // function storeSVGInformation() internal {}
+
+    // function getSVGImage() public pure {}
 }
