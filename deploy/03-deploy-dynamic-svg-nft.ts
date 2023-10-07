b05455b38bca167767451a2e295d33f56d92317b
Dustin Stacy <dustin.stacy@outlook.com>
2023-10-07 10:31:46 -0400

    Add ETHUSDPriceFeedAddress deployment

--- deploy/03-deploy-dynamic-svg-nft.ts
+++ deploy/03-deploy-dynamic-svg-nft.ts
@@ -1,11 +1,21 @@
-import { network } from 'hardhat'
+import { ethers, network } from 'hardhat'
 import { devChains, networkConfig } from '../helper-hardhat-config'
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 import verify from '../utils/verify'
+import { AddressLike } from 'ethers'
 
 const deployDynamicSVGNFT: DeployFunction = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+
+    let ethUSDPriceFeedAddress: string | undefined
+
+    if (devChains.includes(network.name)) {
+        const EthUSDAggregatorAddress = (await deployments.get('MockV3Aggregator')).address
+        ethUSDPriceFeedAddress = EthUSDAggregatorAddress
+    } else {
+        ethUSDPriceFeedAddress = networkConfig[network.name].ethUSDPriceFeed
+    }
 
     log('-------------------------')
     const args: any[] = []
@@ -15,12 +25,6 @@ const deployDynamicSVGNFT: DeployFunction = async ({ getNamedAccounts, deploymen
         log: true,
         waitConfirmations: networkConfig[network.name].blockConfirmations,
     })
-
-    if (!devChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log('Verifying...')
-        await verify(dynamicSVGNFT.address, args)
-    }
-    log('-------------------------')
 }
 
 export default deployDynamicSVGNFT
