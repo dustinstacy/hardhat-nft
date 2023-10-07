0633867ff2864e7fd525aa926ad5e05a827cd6fe
Dustin Stacy <dustin.stacy@outlook.com>
2023-10-07 10:22:33 -0400

    Add MockV3Aggregator deployment

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -4,6 +4,8 @@ import { ethers } from 'hardhat'
 
 const BASE_FEE = ethers.parseEther('0.25')
 const GAS_PRICE_LINK = 1e9
+const DECIMALS = '18'
+const INITIAL_ANSWER = ethers.parseUnits('2000', 'ether')
 
 const deployMocks = async ({ getNamedAccounts, deployments, network }: DeployInterface) => {
     const { deploy, log, get } = deployments
@@ -15,6 +17,11 @@ const deployMocks = async ({ getNamedAccounts, deployments, network }: DeployInt
             from: deployer,
             log: true,
             args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        await deploy('MockV3Aggregator', {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_ANSWER],
         })
     }
     log('Mocks Deployed')
